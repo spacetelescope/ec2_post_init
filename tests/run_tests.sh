646848fd9a3c2e1a840b75b0d53068a5f65898ca
Joseph Hunkeler <jhunkeler@gmail.com>
2022-06-09 12:04:51 -0400

    Run specific test_* scripts as arguments

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,8 +3,17 @@
 export shunit_path="$(pwd)/shunit"
 [ ! -d "$shunit_path" ] && git clone https://github.com/kward/shunit2 "$shunit_path"
 
-for test_script in test_*.sh; do
-    echo "Running tests for: ${test_script}"
-    bash "${test_script}"
-    echo
-done
+argv=($@)
+argc=$#
+
+if (( $argc > 0 )); then
+    for (( i=0; i < $argc; i++ )); do
+        [ -f "${argv[i]}" ] && [[ "${argv[i]}" =~ ^test_.*\.sh ]] && bash "${argv[i]}"
+    done
+else
+    for test_script in test_*.sh; do
+        echo "Running tests for: ${test_script}"
+        bash "${test_script}"
+        echo
+    done
+fi
