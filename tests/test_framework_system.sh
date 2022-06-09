e2683e3c91a782ff0520d661f0de6a4b7489fbc3
Joseph Hunkeler <jhunkeler@gmail.com>
2022-06-09 12:06:16 -0400

    Add system framework tests

---
+++ tests/test_framework_system.sh
@@ -0,0 +1,87 @@
+ec2pinit_root=$(realpath ..)
+if (( $EUID > 0 )); then
+    not_root=1
+fi
+
+oneTimeSetUp() {
+    source $ec2pinit_root/ec2pinit.inc.sh
+    mkdir -p "$ec2pinit_tempdir"/home/tester
+    export HOME_ORIG="$HOME"
+    export HOME="$ec2pinit_tempdir"/home/tester
+    export USER="root"
+}
+
+(( $not_root )) && startSkipping
+
+# this test is a no-op
+test_sys_user_push() {
+    (( $not_root )) && return
+    sys_user_push "$USER" 1>/dev/null
+    assertTrue "" '[[ $HOME == "$HOME_ORIG" ]]'
+}
+
+# this test is a no-op
+test_sys_user_pop() {
+    (( $not_root )) && return
+    sys_user_pop 1>/dev/null
+    assertTrue "" '[[ $HOME == "$HOME_ORIG" ]]'
+}
+
+test_sys_reset_home_ownership() {
+    (( $not_root )) && return
+    sys_reset_home_ownership $USER 1>/dev/null
+    retval=$?
+    assertTrue "Failed to reset ownership" '[ $retval -eq 0 ]'
+}
+
+test_sys_pkg_install() {
+    (( $not_root )) && return
+    pkg=nano
+    sys_pkg_install $pkg 1>/dev/null
+    retval=$?
+    assertTrue "'$pkg' could not be installed" '[ $retval -eq 0 ]'
+
+    sys_pkg_installed $pkg
+    retval=$?
+    assertTrue "'$pkg' not installed"  '[ $retval -eq 0 ]'
+}
+
+test_sys_pkg_clean() {
+    (( $not_root )) && return
+    sys_pkg_clean
+    retval=$?
+    assertTrue "Failed to clean up system packages" '[ $retval -eq 0 ]'
+}
+
+(( $not_root )) && endSkipping
+
+test_sys_pkg_get_manager() {
+    assertTrue "" '[[ -n $(sys_pkg_get_manager) ]]'
+}
+
+test_sys_user_home() {
+    assertTrue "" '[[ $(sys_user_home root) == "/root" ]]'
+}
+
+test_sys_arch() {
+    assertTrue "" '[[ -n $(sys_arch) ]]'
+}
+
+test_sys_platform() {
+    assertTrue "" '[[ -n $(sys_platform) ]]'
+}
+
+test_sys_pkg_installed() {
+    sys_pkg_installed coreutils
+    retval=$?
+    assertTrue "coreutils not installed (unlikely!)"  '[ $retval -eq 0 ]'
+
+    sys_pkg_installed samba
+    retval=$?
+    assertTrue "samba installed when it shouldn't be" '[ $retval -ne 0 ]'
+
+    # No arguments
+    assertFalse sys_pkg_installed
+}
+
+. "$shunit_path"/shunit2
