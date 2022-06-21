c9eee373e39726a0d0bb5c4fbb25d3f7612c8661
Joseph Hunkeler <jhunkeler@gmail.com>
2022-06-21 01:51:21 -0400

    Add sphinx and RTD support

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # ec2 post init
 
-[![CI](https://github.com/spacetelescope/ec2_post_init/actions/workflows/main.yml/badge.svg)](https://github.com/spacetelescope/ec2_post_init/actions/workflows/main.yml)
+[![CI](https://github.com/spacetelescope/ec2_post_init/actions/workflows/main.yml/badge.svg)](https://github.com/spacetelescope/ec2_post_init/actions/workflows/main.yml) [![Documentation Status](https://readthedocs.org/projects/ec2-post-init/badge/?version=latest)](https://ec2-post-init.readthedocs.io/en/latest/?badge=latest)
 
 The goal is simple. `ec2_post_init` can be used to populate STScI specific EC2 instances with common software.
-
-# API
-
-TODO
---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+import subprocess
+
+
+subprocess.run("doxygen", shell=True)
+
+# -- Project information -----------------------------------------------------
+
+project = 'ec2_post_init'
+copyright = '2022, Space Telescope Science Institute'
+author = 'Joseph Hunkeler'
+
+html_extra_path = ["html"]
+
+# -- General configuration ---------------------------------------------------
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
